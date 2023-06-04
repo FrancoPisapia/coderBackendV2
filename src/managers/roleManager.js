b685d91fd487b72d49581da2b375129b2e853e27
FrancoPisapia <franco.pisapia405@gmail.com>
2023-06-04 11:41:03 -0300

    manejo de errores

--- src/managers/roleManager.js
+++ src/managers/roleManager.js
@@ -1,4 +1,7 @@
 import RoleMongooseDao from "../dao/roleModelDao.js";
+import idValidation from "../validations/share/idValidation.js";
+import roleCreateValidation from '../validations/roles/roleCreateValidation.js';
+import roleUpdateValidation from '../validations/roles/roleUpdateValidation.js'
 
 class RoleManager
 {
@@ -14,21 +17,25 @@ class RoleManager
 
   async getOne(id)
   {
+    await idValidation.parseAsync({ id });
     return this.roleDao.getOne(id);
   }
 
   async create(data)
   {
+    await roleCreateValidation.parseAsync(data)
     return await this.roleDao.create(data);
   }
 
   async updateOne(id, data)
   {
+    await roleUpdateValidation.parseAsync({...data,id})
     return this.roleDao.updateOne(id, data);
   }
 
   async deleteOne(id)
   {
+    await idValidation.parseAsync({ id });
     return this.roleDao.deleteOne(id);
   }
 }
